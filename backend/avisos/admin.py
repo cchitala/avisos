e08aa0b1161cb4a7e6b7250cfa3d5926f3606525
cchitala <cchitala@gmail.com>
2021-01-19 14:24:44 -0600

    Agregando modelos Estado, Aviso y Nota

--- backend/avisos/admin.py
+++ backend/avisos/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Area, Prioridad
+from .models import Area, Prioridad, Estado, Aviso, Nota
 
-admin.site.register(Area)
-admin.site.register(Prioridad)
+class AreaAdmin(admin.ModelAdmin):
+  list_display = ('area', 'descripcion','estatus')
+admin.site.register(Area, AreaAdmin)
+
+class PrioridadAdmin(admin.ModelAdmin):
+  list_display = ('prioridad','orden')
+admin.site.register(Prioridad, PrioridadAdmin)
+
+admin.site.register(Estado)
+admin.site.register(Aviso)
+admin.site.register(Nota)
+
+
--- backend/avisos/models.py
+++ backend/avisos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Area(models.Model):
   area = models.CharField(max_length=255)
@@ -18,3 +19,32 @@ class Prioridad(models.Model):
 
   class Meta:
       verbose_name_plural = "Prioridades"
+
+class Estado(models.Model):
+  estado = models.CharField(max_length=100)
+  descripcion = models.CharField(max_length=100)
+  status = models.SmallIntegerField(default=1) 
+
+  def __str__(self):
+    return self.estado
+
+class Aviso(models.Model):
+  creador = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+  aviso = models.CharField(max_length=255)
+  descripcion = models.TextField()
+  estado = models.ForeignKey(Estado, blank=True, null=True, on_delete=models.SET_NULL)
+  areas = models.ManyToManyField(Area)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.aviso
+
+class Nota(models.Model):
+  aviso = models.ManyToManyField(Aviso)
+  nota = models.TextField()
+  creador = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+
+  def __str__(self):
+    return self.aviso
+  
